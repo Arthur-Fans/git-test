963a2a0e030eedb642b000eb2a53ca4ab1f96398
Arthur_Fans <arthur_fans@163.com>
2023-05-27 16:18:32 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @ClassName GitTest
+ * @Description TODO
+ * @Author qinnan fan
+ * @Date 2023/5/27 16:09
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
