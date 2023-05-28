610f973a561bc550696898f2d17689ff3b231927
Arthur_Fans <arthur_fans@163.com>
2023-05-28 10:26:57 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -2,12 +2,13 @@ package com.atguigu.git;
 
 /**
  * @ClassName GitTest
- * @Description TODO
+ * @Description
  * @Author qinnan fan
  * @Date 2023/5/27 16:09
  */
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
